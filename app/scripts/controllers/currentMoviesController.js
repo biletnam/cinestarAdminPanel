fba242c9179ff23c48eca55df5549bba0ed3f59c
Swapnil Kumar <swapnilaryan.nitdgp@gmail.com>
2016-08-10 00:00:52 +0530

    modified page current movies page

--- app/scripts/controllers/currentMoviesController.js
+++ app/scripts/controllers/currentMoviesController.js
@@ -11,4 +11,71 @@
  */
 angular.module('sbAdminApp')
 	.controller('currentMoviesController', function($scope,$position) {
+		$scope.actions=["Add","remove"];
+		$scope.screens=["Screen_1","Screen_2","Screen_3","Screen_4"];
+		$scope.dataScreens = [];
+
+		$scope.organiseScreens = [{
+			screenName:"Screen 1",
+			details:[{
+				movieName:"Batman",
+				startTime:"9:00",
+				endTime:"12:30"
+			},{
+				movieName:"Superman",
+				startTime:"9:00",
+				endTime:"12:30"
+			}]
+		},{
+			screenName:"Screen 2",
+			details:[{
+				movieName:"Batman 2 ",
+				startTime:"9:00",
+				endTime:"12:30"
+			},{
+				movieName:"Superman 2 ",
+				startTime:"9:00",
+				endTime:"12:30"
+			},{
+				movieName:"Batman 3 ",
+				startTime:"9:00",
+				endTime:"12:30"
+			},{
+				movieName:"Superman 3 ",
+				startTime:"9:00",
+				endTime:"12:30"
+			}]
+		}];
+
+		// for(var i=0;i<$scope.screens.length;i++){
+		// 	$scope.organiseScreens = {
+		// 		screenName:"",
+		// 		details:[{
+		// 			movieName:"",
+		// 			startTime:"",
+		// 			endTime:""
+		// 		}]
+		// 	};
+		// 	$scope.organiseScreens.screenName = $scope.screens[i];
+		// 	$scope.dataScreens.push($scope.organiseScreens);
+		// }
+		console.log($scope.organiseScreens);
+		// $scope.data = [{
+		// 	id:"odd",
+		// 	screen:"Screen 1",
+		// 	details:{
+		// 		movieName:"Batman",
+		// 		startTime:"9:00",
+		// 		endTime:"12:30"
+		// 	}
+		// },{
+		// 	id:"even",
+		// 	screen:"Screen 2",
+		// 	details:{
+		// 		movieName:"Batman",
+		// 		startTime:"9:00",
+		// 		endTime:"12:30"
+		// 	}
+		// }
+		// ];
 	});
