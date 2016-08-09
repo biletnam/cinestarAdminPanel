377c08a36569591e1e09a3cacbfd76a70486702f
Swapnil Kumar <swapnilaryan.nitdgp@gmail.com>
2016-08-09 07:57:19 +0530

    Initial commit for admin panel

---
+++ app/scripts/controllers/currentMoviesController.js
@@ -0,0 +1,14 @@
+/**
+ * Created by !!.Swapnil..Aryan.!! on 08-Aug-16.
+ */
+'use strict';
+/**
+ * @ngdoc function
+ * @name sbAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sbAdminApp
+ */
+angular.module('sbAdminApp')
+	.controller('currentMoviesController', function($scope,$position) {
+	});
---
+++ app/scripts/controllers/upcoming-movies.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sbAdminApp.controller:UpcomingMoviesCtrl
+ * @description
+ * # UpcomingMoviesCtrl
+ * Controller of the sbAdminApp
+ */
+angular.module('sbAdminApp')
+  .controller('UpcomingMoviesCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
