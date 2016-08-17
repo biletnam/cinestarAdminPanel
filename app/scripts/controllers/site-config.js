5948d4e53c371823406a59cfb179963184f7c9b6
Swapnil Kumar <swapnilaryan.nitdgp@gmail.com>
2016-08-18 00:56:34 +0530

    site config done * Libraries outdated .... need to think about it

--- app/scripts/controllers/site-config.js
+++ app/scripts/controllers/site-config.js
@@ -8,10 +8,20 @@
  * Controller of the sbAdminApp
  */
 angular.module('sbAdminApp')
-  .controller('SiteConfigCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('SiteConfigCtrl', function ($scope) {
+      $scope.daysInWeek = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+      $scope.siteConfig = {
+          theatreName:"Cinestar",
+          websiteUrl:"cinex.press",
+          timeZone:"Huntsville"
+      };
+      $scope.openCloseTime=[
+      {   Monday:["19:00","20:00"]},
+      {   Tuesday:["9:00","18:30"]},
+      {   Wednesday:["9:00","19:00"]},
+      {   Thursday:["9:00","19:30"]},
+      {   Friday:["9:00","18:00"]},
+      {   Saturday:["9:00","21:00"]},
+      {   Sunday:["9:00","22:00"]}
+      ]
   });
