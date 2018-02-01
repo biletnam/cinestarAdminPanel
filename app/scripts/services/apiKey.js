c0b9f9b72c6a0e7dc28e5c3be2cc12e305c1f4b2
Swapnil Kumar <swapnil.kumar@blackbuck.com>
2018-02-01 10:03:44 +0530

    port changed

--- app/scripts/services/apiKey.js
+++ app/scripts/services/apiKey.js
@@ -22,7 +22,7 @@ angular.module('sbAdminApp')
 				}
 				else {
 					// apiUrl = "http://cinestar.affpc.com:8080/api/";
-					apiUrl = "http://18.217.132.65:8001/api/";
+					apiUrl = "http://18.217.132.65:8000/api/";
 				}
 				return apiUrl;
 			},
