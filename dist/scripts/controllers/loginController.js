f3b7c6bccdd2e5a523f7e28b74038b7a83503352
Swapnil Kumar <swapnil.kumar@blackbuck.com>
2018-02-07 09:00:12 +0530

    asjdhakjshd

--- dist/scripts/controllers/loginController.js
+++ dist/scripts/controllers/loginController.js
@@ -1,1 +1,16 @@
-"use strict";angular.module("sbAdminApp").controller("loginController",function($scope,apiKey,$stateParams,admin_login_register,$state,$cookieStore,$rootScope){$scope.openSignUp=function(){$state.go("register")},$scope.register={emailId:"",password:""},$scope.$on("signinWithSignUp",function(event,data){console.log("---------------------",data),$scope.register.emailId=data.adminUserEmail,$scope.register.password=data.password,$scope.signinUser()}),$scope.signinUser=function(){$scope.data={adminUserEmail:$scope.register.emailId,password:$scope.register.password},admin_login_register.postLogin($scope.data).then(function(resolve,reject){console.log(resolve),console.log(reject),"Fail"!=resolve.data.Status?($state.go("dashboard.home"),$scope.userLoggedIn=!0,$cookieStore.put("userLogin",$scope.register.emailId)):$scope.userLoggedIn=!1})}});
+"use strict";
+angular.module("sbAdminApp").controller("loginController", function ($scope, apiKey, $stateParams, admin_login_register, $state, $cookieStore, $rootScope) {
+    $scope.openSignUp = function () {
+        $state.go("register")
+    }, $scope.register = {emailId: "", password: ""}, $scope.$on("signinWithSignUp", function (event, data) {
+        localStorage.setItem("user_session", data.data.message);
+        console.log("---------------------", data), $scope.register.emailId = data.adminUserEmail, $scope.register.password = data.password, $scope.signinUser()
+    }), $scope.signinUser = function () {
+        $scope.data = {
+            adminUserEmail: $scope.register.emailId,
+            password: $scope.register.password
+        }, admin_login_register.postLogin($scope.data).then(function (resolve, reject) {
+            console.log(resolve), console.log(reject), "Fail" != resolve.data.Status ? ($state.go("dashboard.home"), $scope.userLoggedIn = !0, $cookieStore.put("userLogin", $scope.register.emailId)) : $scope.userLoggedIn = !1
+        })
+    }
+});
