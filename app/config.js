16b217da3fcc51d7c4f82434cf798ba4843a485b
Swapnil Kumar <swapnil.kumar@blackbuck.com>
2018-02-11 04:16:24 +0530

    clonig working good

---
+++ app/config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by swapnil on 11/02/18.
+ */
+var config = {
+    theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
+    apiUrl: 'http://localhost:8000/api',
+    imagePath: 'http://127.0.0.5/',
+    theMovieDBURL:'http://api.themoviedb.org/3/'
+};
