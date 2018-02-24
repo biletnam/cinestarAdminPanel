f3cc51d2617a3d561895782e85245d173fbf8306
Swapnil Kumar <swapnil.kumar@blackbuck.com>
2018-02-25 02:39:57 +0530

    changing port

--- app/config.js
+++ app/config.js
@@ -3,7 +3,7 @@
  */
 var config = {
     theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
-    apiUrl: 'http://localhost:8000/api',
+    apiUrl: 'http://localhost:9000/api',
     imagePath: 'https://image.tmdb.org/t/p',
     theMovieDBURL:'http://api.themoviedb.org/3/'
 };
