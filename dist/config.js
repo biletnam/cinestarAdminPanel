df54ebc0c4e993285abd1796ca6a400317f8bc22
Swapnil Kumar <swapnil.kumar@blackbuck.com>
2018-02-25 03:56:38 +0530

    Dist

--- dist/config.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by swapnil on 11/02/18.
- */
-var config = {
-    theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
-    apiUrl: 'http://localhost:9000/api',
-    imagePath: 'https://image.tmdb.org/t/p',
-    theMovieDBURL:'http://api.themoviedb.org/3/'
-};
