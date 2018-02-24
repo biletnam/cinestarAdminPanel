638832f61e17a47cbcfc03f5c873280fed659e3d
Swapnil Kumar <swapnil.kumar@blackbuck.com>
2018-02-25 03:58:21 +0530

    grut sit

---
+++ dist/config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by swapnil on 11/02/18.
+ */
+var config = {
+    theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
+    apiUrl: 'http://localhost:9000/api',
+    imagePath: 'https://image.tmdb.org/t/p',
+    theMovieDBURL:'http://api.themoviedb.org/3/'
+};
