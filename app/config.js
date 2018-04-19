da85ec5c098888cd98168ceedd9f061a410d4f8b
Swapnil Kumar <swapnil.kumar@blackbuck.com>
2018-04-19 13:27:06 +0530

    Config change s

--- app/config.js
+++ app/config.js
@@ -3,17 +3,20 @@
  */
 
 //
-// var config = {
-//     theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
-//     apiUrl: 'http://localhost:9000/api',
-//     imagePath: 'https://image.tmdb.org/t/p',
-//     theMovieDBURL:'http://api.themoviedb.org/3/'
-// };
-
 var config = {
     theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
-    apiUrl: 'http://18.218.55.255:9000/api',
+    apiUrl: 'http://localhost:9000/api',
     imagePath: 'https://image.tmdb.org/t/p',
     theMovieDBURL:'http://api.themoviedb.org/3/',
-    imageSize: '/w154/'
+    imageSize: '/w154/',
+    baseUrl: 'http://localhost:9000'
 };
+
+// var config = {
+//     theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
+//     apiUrl: 'http://18.218.55.255:9000/api',
+//     imagePath: 'https://image.tmdb.org/t/p',
+//     theMovieDBURL:'http://api.themoviedb.org/3/',
+//     imageSize: '/w154/'
+//     baseUrl: 'http://18.218.55.255:8000'
+// };
--- app/scripts/services/apiKey.js
+++ app/scripts/services/apiKey.js
@@ -22,7 +22,7 @@ angular.module('sbAdminApp')
 				}
 				else {
 					// apiUrl = "http://cinestar.affpc.com:8080/api/";
-					apiUrl = "http://18.218.55.255:8000/api/";
+					apiUrl = config.baseUrl;//"http://18.218.55.255:8000/api/";
 				}
 				return apiUrl;
 			},
@@ -32,7 +32,7 @@ angular.module('sbAdminApp')
 					image = "http://127.0.0.5/";
 				}
 				else {
-					image = "http://18.218.55.255:8000/";
+					image = config.baseUrl;//"http://18.218.55.255:8000/";
 				}
 				return image;
 			}
